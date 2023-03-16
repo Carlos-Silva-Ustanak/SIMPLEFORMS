84236ba707ebea7ab0ed21cdd2390c6d2b625eec
Carlos Freitas <122635080+Carlos-Silva-Ustanak@users.noreply.github.com>
2023-03-16 20:06:17 -0300

    att js

---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+let emailin = document.getElementById("email");
+let passin = document.getElementById("pass");
+let btn = document.getElementById("BOTAO");
+let emaillabel= document.getElementById("emaillabel");
+let senhalabel = document.getElementById("senhaLabel");
+
+if(emailin.value && passin.value == ''){
+
+btn.disabled = true;
+
+
+}
+
+function entrar(e){
+
+
+
+let users = []
+
+let usuariosValidos = {
+
+
+email : '#',
+senha : '#'
+
+}
+
+users = JSON.parse(localStorage.getItem('users'))
+
+
+users.forEach((item) => {
+    if(email.value == item.email && pass.value == item.senha){
+
+        usuariosValidos = {
+
+            email: item.email,
+            senha: item.senha
+           
+        }
+ 
+    }
+});
+
+if(emailin.value == usuariosValidos.email && passin.value == usuariosValidos.senha){
+
+window.open('../html/logged.html')
+let token = Math.random().toString(16).substr(2)
+console.log(token)
+localStorage.setItem('token', token)
+}
+
+
+else{
+
+    Swal.fire({
+        title: 'PRENCHA CORRETAMENTE',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      })
+
+}
+
+}
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -3,20 +3,66 @@ let Cpass = document.getElementById("Cpass");
 let btn = document.getElementById("BOTAO");
 let emaillabel= document.getElementById("emaillabel");
 let Cpasslabel = document.getElementById("CpassLabel");
-
+let senhalabel = document.getElementById("senhaLabel")
+let email = document.getElementById("email");
+btn.disabled = true;
 
 Cpass.addEventListener('keyup', () =>{
 
-if(pass.value != Cpass.value ){
+if(pass.value != Cpass.value){
 
-Cpasslabel.innerHTML = "senhas não compativeis"
+Cpasslabel.innerHTML = "SENHAS NÃO COMPATIVEIS"
 btn.disabled = true;
+Cpasslabel.setAttribute('style', 'color: red')
 
-}else{
+}
+
+else if(Cpass.value == pass.value){
 
     btn.disabled = false;
     Cpasslabel.innerHTML = "SENHA";
+    Cpasslabel.setAttribute('style', 'color: black')
 
 }
 
 })
+
+pass.addEventListener('keyup', () =>{
+
+    if(Cpass.value == '' || pass.value == '' || email.value == ''){
+
+        btn.disabled = true;
+        Cpasslabel.innerHTML = "SENHA"
+        Cpasslabel.setAttribute('style', 'color: black')
+        
+    }
+    
+    else{
+    
+        btn.disabled = false;
+        Cpasslabel.innerHTML = "SENHA";
+        Cpasslabel.setAttribute('style', 'color: black')
+    
+    }
+    
+    })
+
+    btn.addEventListener('click', ()=>{
+
+        location.href = '//youtube.com'
+     
+
+        let users = JSON.parse(localStorage.getItem('users') || '[]')
+
+        users.push({
+            email : email.value,
+            senha : pass.value
+        })
+
+        localStorage.setItem('users', JSON.stringify(users))
+     
+        
+          
+    })
+
+   
