5e029eb7ed39461e25edd961fe9a0a1a3f5ce9b2
Carlos Freitas <122635080+Carlos-Silva-Ustanak@users.noreply.github.com>
2023-03-22 23:11:55 -0300

    almost done

--- assets/js/logged.js
+++ assets/js/logged.js
@@ -1,4 +1,3 @@
-/* VERIFICAR SE HÁ UM TOKEN NO LOCALSTORAGE E ENTÃO EFETUAR O LOGIN */
 if (localStorage.getItem('token') == null) {
 
     window.location.href = '../html/index.html'
@@ -13,143 +12,108 @@ function logOut() {
 
 }
 
-/* VARIAVEIS DOS INPUTS */
-let forms = document.getElementById('forms');
-let tabela = document.getElementById('tabela');
-let saveButton = document.getElementById('savebutton');
-let produtos = document.getElementById('produtos')
-let preco = document.getElementById('preco')
-let prime = document.getElementById('prime')
-saveButton.disabled = true;
+const forms = document.querySelector("#forms");
+const tabela = document.querySelector("#tabela");
+const btn = document.querySelector("#savebutton");
+let idx = forms.idx.value;
 
-/*EVENTO PARA GARANTIR QUE NÃO SEJAM ENVIADOS INPUTS VAZIOS PARA O LOCALSTORAGE */
-saveButton.disabled = true;
+const attLocalStorage = (funcionarios)=>{localStorage.setItem('funcionarios', JSON.stringify(funcionarios))}
 
-produtos.addEventListener('keyup', () => {
+const recuperarLocalStorage =()=> JSON.parse(localStorage.getItem('funcionarios') || '[]') 
 
-    if (produtos.value == '' || preco.value == '') {
+const salvarFuncionario = (e)=>{
+e.preventDefault()
 
-        saveButton.disabled = true;
+const funcionarioNome = forms.funcio.value;
+const cargo = forms.cargo.value;
+const proativ = forms.proativ.checked;
 
-    }
+if(idx == "novo"){
 
-    else {
+    const funcionarios = recuperarLocalStorage();
+    funcionarios.push({id: funcionarios.length + 1, funcionarioNome, cargo, proativ})
+    attLocalStorage(funcionarios);
+    preencherTabela();
+    forms.reset();    
 
-        saveButton.disabled = false;
+}else{
 
-    }
-
-}
-
-)
-
-/*EVENTO PARA GARANTIR QUE NÃO SEJAM ENVIADOS INPUTS VAZIOS PARA O LOCALSTORAGE */
-preco.addEventListener('keyup', () => {
-
-    if (produtos.value == '' || preco.value == '') {
-
-        saveButton.disabled = true;
-
-    }
-
-    else {
-
-        saveButton.disabled = false;
-
-    }
+let funcionario = {id: idx, funcionarioNome, cargo, proativ }
+editar(id = idx,  funcionario);
+preencherTabela();
+forms.reset();
+idx = "novo";
 
 }
 
-)
 
 
-/* SALVAMENTO DOS VALORES DOS INPUTS NO LOCALSTORAGE */
-saveButton.addEventListener('click', (e) => {
-
-e.preventDefault();
-
-let products = JSON.parse(localStorage.getItem('produtos') || '[]')
-
-    let produ = {
-
-        id: products.length + 1,
-        pro: produtos.value,
-        price: preco.value,
-        pri: prime.checked
-
-    }
-
- 
-
-        products.push(produ);
-        localStorage.setItem('produtos', JSON.stringify(products))
-        forms.reset();
-        attTable();
-})
-
-
-/* ATUALIZAÇÃO DA TABELA */
-let attTable = () => {
-
-    let products = JSON.parse(localStorage.getItem('produtos') || '[]')
-
-    tabela.innerHTML = '';
-
-    for (let produtos of products) {
-
-        tabela.innerHTML += `
-
-            <tr>
-            
-                <th scope = "row">${produtos.id}</th>
-                <td style = "text-align: center">${produtos.pro}</td>
-                <td style = "text-align: center">R$${produtos.price}</td>
-                <td style = "text-align: center">${produtos.pri ? "sim" : "não"}</td>
-                <td style = "text-align: center">
-                <img type = "button" width = "40px"  onclick = "attItem(${produtos.id})" src ="../img/editar-arquivo.png"></img>
-                <img type = "button" width = "40px"  onclick = "removeitem(${produtos.id})" src ="../img/bin.png"></img>                
-                </td>
-                
-            </tr>
-
-`;
-
-    }
 
 }
 
-/* REMOVER ITEM NA TABELA */
-let removeitem = (id)=>{
+const preencherTabela = ()=>{
 
-let local = JSON.parse(localStorage.getItem('produtos') || '[]')
-let indexProduto = local.findIndex((local)=> local.id === id)
-if(indexProduto < 0 )return;
-local.splice(indexProduto, 1)
-localStorage.setItem('produtos', JSON.stringify(local))
-attTable();
+const funcionarios = recuperarLocalStorage();
+tabela.innerHTML = '';
+
+for(const fun of funcionarios){
+
+tabela.innerHTML += `
+
+<tr>
+
+<th scope="row">${fun.id}</th>
+<td style = "text-align:center">${fun.funcionarioNome}</td>
+<td style = "text-align:center">${fun.cargo}</td>
+<td scope="row" style = "text-align:center">${fun.proativ ? "SIM" : "NÃO"} </td>
+<td>
+
+<img type = "button" width = "40" src = "../img/editar-arquivo.png" onclick = "attFuncionarios(${fun.id})">
+<img type = "button" width = "40" src = "../img/bin.png" onclick ="removerFuncionario(${fun.id}) ">
+
+
+</td>
+
+</tr>
+
+
+` ;
+
 
 }
 
-let attItem = (id)=>{
-
-    let local = JSON.parse(localStorage.getItem('produtos') || '[]')
-    let indexProduto = local.findIndex((local)=> local.id === id)
-
-    forms.produto.value = local[indexProduto].pro
-    forms.preco.value = local[indexProduto].price;
-    forms.prime.checked = local[indexProduto].pri
-    idx = id;
 
 }
 
-let editItem = (id, produtos)=>{
-    let local = JSON.parse(localStorage.getItem('produtos') || '[]')
-    let indexProduto = local.findIndex((local)=> local.id === id)
+const removerFuncionario = (id)=> {
 
-    local[indexProduto] = produto;
-    localStorage.setItem('produtos', JSON.stringify(local))
+const data = recuperarLocalStorage();
+const indexFuncionario = data.findIndex((funcionarios) => funcionarios.id === id);
+if(indexFuncionario < 0 )return;
+data.splice(indexFuncionario, 1);
+attLocalStorage(data);
+preencherTabela();
 }
 
+const attFuncionarios = (id)=>{
 
-/* SALVAR CONTEÚDO ATUALIZADO NA TABELA */
-document.addEventListener("DOMContentLoaded", attTable());
+     const data = recuperarLocalStorage();
+     const indexFuncionario = data.findIndex((funcionarios) => funcionarios.id === id);
+     forms.funcio.value = data[indexFuncionario].funcionarioNome;
+     forms.cargo.value = data[indexFuncionario].cargo;
+     forms.proativ.checked = data[indexFuncionario].proativ;
+     idx = id;
+}
+
+const editar = (id, funcionario)=>{
+
+    const data = recuperarLocalStorage();
+    const indexFuncionario = data.findIndex((funcionario) => funcionario.id === id);
+    data[indexFuncionario] = funcionario;
+    attLocalStorage(data);
+
+
+}
+
+forms.addEventListener('submit', salvarFuncionario)
+document.addEventListener('DOMContentLoaded', preencherTabela)
